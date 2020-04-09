6378980b681dcf0c72fe847d10df963972afc0bf
Josh Archer <jarcher@greenriver.edu>
2020-04-09 12:02:04 -0700

    Added an example of assigning variables (WA state facts).

---
+++ variables_data_types/wa_facts/scripts/facts.js
@@ -0,0 +1,24 @@
+/*
+    This is a script which enters WA state facts into our HTML page.
+    Author: Josh archer
+    Date: 4/9/20
+    File: facts.js
+ */
+
+//access a list item and update the content of the li
+var fact1 = document.querySelector("li#fact-1");
+fact1.innerHTML = "The Lewis and Clark expedition entered the state on October 10, 1805.";
+var fact2 = document.querySelector("li#fact-2");
+fact2.innerHTML = "By the turn of the 20th century, Aberdeen had the distinction of " +
+    "being 'the roughest town west of the Mississippi' because of excessive gambling, " +
+    "violence, extreme drug use and prostitution (the city remained off-limits to " +
+    "military personnel into the early 1980s).";
+var fact3 = document.querySelector("li#fact-3");
+fact3.innerHTML = "The first settlement in Washington was New Market (now known as Tumwater) in 1846.";
+
+var fact4 = document.querySelector("li#fact-4");
+fact4.innerHTML = "In 1853, Washington Territory was formed from part of Oregon Territory.";
+var fact5 = document.querySelector("li#fact-5");
+fact5.innerHTML = "Washington became the 42nd state in the United States on November 11, 1889.";
+var fact6 = document.querySelector("li#fact-6");
+fact6.innerHTML = "Residents are called “Washingtonians” (emphasis on the third syllable, pronounced as tone).";
