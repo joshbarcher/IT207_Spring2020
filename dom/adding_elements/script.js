c17abf8c1a9c431be78257c35e4343c5dd06c4a0
Josh Archer <jarcher@greenriver.edu>
2020-05-19 10:34:41 -0700

    Added an example of adding elements to the DOM.

---
+++ dom/adding_elements/script.js
@@ -0,0 +1,16 @@
+
+let ul = document.querySelector("ul");
+
+//try to avoid this...
+//ul.innerHTML += "<li>Abraham Lincoln</li>";
+
+//try to prefer this mechanism...
+let li = document.createElement("li");
+li.innerHTML = "Martin Luther King ";
+
+let anchor = document.createElement("a");
+anchor.innerHTML = "Learn More";
+anchor.setAttribute("href", "https://www.history.com/this-day-in-history/martin-luther-king-jr-born");
+
+li.appendChild(anchor);
+ul.appendChild(li);
